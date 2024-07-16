4cf4e19bb55c9ee54ef3fc509e33b383542be8b4
jl917 <julong1988@naver.com>
2024-07-16 21:28:22 +0900

    feat: initialize

---
+++ src/cookie.ts
@@ -0,0 +1,47 @@
+const domainParts = window.location.hostname.split(".");
+
+function _deleteCookie(name: string, path: string = "/", domain?: string) {
+  document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=${path}${
+    domain ? `; domain=${domain}` : ""
+  }`;
+}
+
+function getAllCookies() {
+  const cookies = document.cookie.split(";").map((cookie) => cookie.trim());
+  return cookies.map((cookie) => {
+    const [name, ...valueParts] = cookie.split("=");
+    return { name, value: valueParts.join("=") };
+  });
+}
+
+function deleteCookie(name: string) {
+  const cookies = getAllCookies();
+  cookies.forEach((cookie) => {
+    if (cookie.name === name) {
+      _deleteCookie(name, "/");
+      _deleteCookie(name, "/", window.location.hostname);
+      while (domainParts.length > 1) {
+        domainParts.shift();
+        _deleteCookie(name, "/", "." + domainParts.join("."));
+      }
+    }
+  });
+}
+
+function deleteAllCookie() {
+  const cookies = getAllCookies();
+  cookies.forEach((cookie) => {
+    _deleteCookie(cookie.name, "/");
+    _deleteCookie(cookie.name, "/", window.location.hostname);
+    while (domainParts.length > 1) {
+      domainParts.shift();
+      _deleteCookie(cookie.name, "/", "." + domainParts.join("."));
+    }
+  });
+}
+
+export default {
+  getAllCookies,
+  deleteCookie,
+  deleteAllCookie,
+};
