b6f032fb0ff655d58543b5859b69f950af3baf09
jl917 <julong1988@naver.com>
2024-08-18 21:58:30 +0900

    fix: checkHTML 오류 수정

--- src/htmlCheck.ts
+++ src/htmlCheck.ts
@@ -1,15 +1,22 @@
 import { loadScript } from "./utils";
 
+let isFirst = true;
 const checkHtml = () => {
   const urls = [
     "https://unpkg.com/htmlhint@latest/dist/htmlhint.js",
     "https://cdn.jsdelivr.net/npm/htmlhint@latest/dist/htmlhint.js",
   ];
-  loadScript(urls, () => {
-    window.HTMLHint.HTMLHint.verify(
-      document.doctype + document.documentElement.outerHTML
-    );
-  });
+
+  const cb = () => {
+    console.log(window.HTMLHint.HTMLHint.verify(document.doctype + document.documentElement.outerHTML));
+  };
+
+  if (isFirst) {
+    isFirst = false;
+    loadScript(urls, cb);
+  } else {
+    cb();
+  }
 };
 
 export { checkHtml };
