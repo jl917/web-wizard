ca62892facb6f73231fe2be345719a9c4d01c4f2
jl917 <julong1988@naver.com>
2024-08-18 22:10:32 +0900

    fix: html error 가져오기 및 오류 수정

--- src/htmlCheck.ts
+++ src/htmlCheck.ts
@@ -1,6 +1,16 @@
 import { loadScript } from "./utils";
 
 let isFirst = true;
+
+const getDocType = () => {
+  const node = document.doctype as DocumentType;
+  var html = `<!DOCTYPE ${node.name}${node.publicId ? ` PUBLIC "${node.publicId}"` : ""}${
+    !node.publicId && node.systemId ? " SYSTEM" : ""
+  }${node.systemId ? ` "${node.systemId}"` : ""}>`;
+
+  return html;
+};
+
 const checkHtml = () => {
   const urls = [
     "https://unpkg.com/htmlhint@latest/dist/htmlhint.js",
@@ -8,7 +18,7 @@ const checkHtml = () => {
   ];
 
   const cb = () => {
-    console.log(window.HTMLHint.HTMLHint.verify(document.doctype + document.documentElement.outerHTML));
+    console.log(window.HTMLHint.HTMLHint.verify(getDocType() + document.documentElement.outerHTML));
   };
 
   if (isFirst) {
@@ -19,4 +29,8 @@ const checkHtml = () => {
   }
 };
 
-export { checkHtml };
+const getHtmlError = () => {
+  window.HTMLHint.HTMLHint.verify(getDocType() + document.documentElement.outerHTML);
+};
+
+export { checkHtml, getHtmlError };
